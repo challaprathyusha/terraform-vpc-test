1fa57843a5fa06a8efe7640e9679eb6e163e5f47
challa prathyusha <cprathyusha94@gmail.com>
2024-08-07 10:54:45 -0500

    mssg

--- vpc-test.tf
+++ vpc-test.tf
@@ -1,5 +1,7 @@
 module "vpc-test" {
     #source = "../terraform-aws-vpc-module"
+    #referring vpc module from github
+    #terraform downloads vpc module from github when we initialize the terraform 
     source = "git::https://github.com/challaprathyusha/terraform-aws-vpc-module.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
@@ -7,4 +9,5 @@ module "vpc-test" {
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = var.is_peering_required
+    environment = "dev"
 }
